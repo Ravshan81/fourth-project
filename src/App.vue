d4fcf9dc54568b6208c1d25b5877bb784dd5f9ce
Ravshan81 <tajihadjayevr@gmail.com>
2024-10-21 11:04:25 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="pt-[98px] bg-[#EFC356]">
+    <div class="container">
+      <div class="flex gap-[88px] items-center">
+        <div>
+          <h1
+            class="font-extrabold text-[80px] leading-[88px] text-white max-w-[700px] w-full"
+          >
+            We take good care of your pet
+          </h1>
+          <p
+            class="mt-[40px] max-w-[580px] w-full font-normal text-[24px] leading-[32px] text-white"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus
+            imperdiet sed id elementum. Quam vel aliquam sit vulputate. Lorem
+            ipsum dolor sit amet, consectetur.
+          </p>
+          <button class="py-[24px] px-[64px] bg-[#7EB5E1] mt-[40px] rounded-[40px] font-medium text-[24px] leading-[18px] text-white">Schedule a visit</button>
+        </div>
+        <img src="../public/Group 1.png" alt="">
+      </div>
+      <div class="p-[40px] bg-[#EFEEEE] rounded-[24px] relative top-[5rem] flex gap-[78px] justify-center">
+        <div class="flex gap-[32px]">
+          <img src="../public/Frame 14354.png" alt="">
+          <div>
+            <h4 class="font-bold text-[28px]">Title 1</h4>
+            <p class="mt-[4px] max-w-[210px] w-full font-normal text-[16px] leading-[19px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+        <div class="flex gap-[32px]">
+          <img src="../public/Frame 14354 (1).png" alt="">
+          <div>
+            <h4 class="font-bold text-[28px]">Title 2</h4>
+            <p class="mt-[4px] max-w-[210px] w-full font-normal text-[16px] leading-[19px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+        <div class="flex gap-[32px]">
+          <img src="../public/Frame 14354 (2).png" alt="">
+          <div>
+            <h4 class="font-bold text-[28px]">Title 3</h4>
+            <p class="mt-[4px] max-w-[210px] w-full font-normal text-[16px] leading-[19px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+/* Add any custom styles here if needed */
+</style>
